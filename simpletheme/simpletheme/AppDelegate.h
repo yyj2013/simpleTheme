e5fc2d47079c86e1606f21e42d0879a56b1e95f2
yanyongjie <yanyongjie@live.cn>
2016-08-11 21:28:00 +0800

    project init

---
+++ simpletheme/simpletheme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  simpletheme
+//
+//  Created by csyyj on 16/8/11.
+//  Copyright © 2016年 imudges. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ simpletheme/simpletheme/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  simpletheme
+//
+//  Created by csyyj on 16/8/11.
+//  Copyright © 2016年 imudges. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
